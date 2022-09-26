4f7ca8d5280bfff075e38fdb6afdf2d01f225cea
bladewind21 <bladewind21@gmail.com>
2022-09-26 10:40:39 -0500

    adding configuration for mongodb repository

--- src/main/java/com/review/core/dto/User.java
+++ src/main/java/com/review/core/dto/User.java
@@ -4,15 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.persistence.*;
 import java.util.List;
+import java.util.Set;
 
 @Data
 @Entity
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Document(collection = "user")
 public class User {
 
     @Id
@@ -32,4 +35,7 @@ public class User {
     List<ContactInformation> contactInformation;
     @OneToMany(fetch = FetchType.LAZY)
     List<Pto> pto;
+
+
+
 }
