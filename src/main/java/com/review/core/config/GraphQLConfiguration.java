0479fcda087d7625aa598bdc94cb73468df633e6
bladewind21 <bladewind21@gmail.com>
2022-09-28 22:15:03 -0500

    attempting to dockerize

--- src/main/java/com/review/core/config/GraphQLConfiguration.java
+++ src/main/java/com/review/core/config/GraphQLConfiguration.java
@@ -8,6 +8,7 @@ import com.review.core.service.GraphQLUserService;
 import io.leangen.graphql.metadata.strategy.query.PublicResolverBuilder;
 import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
 import io.leangen.graphql.spqr.spring.annotations.WithResolverBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
@@ -18,19 +19,27 @@ import java.util.Collection;
 @Configuration
 public class GraphQLConfiguration extends AbstractMongoClientConfiguration {
 
+    @Value("${spring.data.mongodb.uri}")
+    private String mongoUri;
+
+    @Value("${spring.data.mongodb.database}")
+    private String mongoDatabase;
+
+    @Value("${spring.data.mongodb.base-packages}")
+
     @Override
     protected String getDatabaseName() {
-        return "review";
+        return mongoDatabase;
     }
 
     @Override
     public MongoClient mongoClient() {
-        ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017");
-        MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+        ConnectionString connectionString = new ConnectionString(mongoUri);
+        MongoClientSettings settings = MongoClientSettings.builder()
                 .applyConnectionString(connectionString)
                 .build();
 
-        return MongoClients.create(mongoClientSettings);
+        return MongoClients.create(settings);
     }
 
     @Override
